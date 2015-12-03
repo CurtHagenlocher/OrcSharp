6fe6e06c2b56d6266020ad4bb1df9c6cb5ca2778
Curt Hagenlocher <curt@hagenlocher.org>
2015-12-03 07:35:18 -0800

    Added (failing) test for vectorized reader

--- OrcSharpTests/TestVectorizedORCReader.cs
+++ OrcSharpTests/TestVectorizedORCReader.cs
@@ -16,234 +16,212 @@
  * limitations under the License.
  */
 
-namespace org.apache.hadoop.hive.ql.io.orc {
+namespace OrcSharp
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using OrcSharp.Serialization;
+    using OrcSharp.Types;
+    using Xunit;
 
-import java.io.File;
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Random;
+    /**
+    *
+    * Class that tests ORC reader vectorization by comparing records that are
+    * returned by "row by row" reader with batch reader.
+    *
+    */
+    public class TestVectorizedORCReader : WithLocalDirectory
+    {
+        class MyRecord
+        {
+            private bool? bo;
+            private byte? by;
+            private int? i;
+            private long? l;
+            private short? s;
+            private double? d;
+            private string k;
+            private Timestamp? t;
+            private Date? dt;
+            private HiveDecimal hd;
 
-import junit.framework.Assert;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hive.common.type.HiveDecimal;
-import org.apache.hadoop.hive.ql.exec.vector.BytesColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.ColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.DecimalColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.DoubleColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
-import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
-import org.apache.hadoop.hive.serde2.io.ByteWritable;
-import org.apache.hadoop.hive.serde2.io.DateWritable;
-import org.apache.hadoop.hive.serde2.io.DoubleWritable;
-import org.apache.hadoop.hive.serde2.io.HiveDecimalWritable;
-import org.apache.hadoop.hive.serde2.io.ShortWritable;
-import org.apache.hadoop.hive.serde2.io.TimestampWritable;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
-import org.apache.hadoop.io.BooleanWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-/**
-*
-* Class that tests ORC reader vectorization by comparing records that are
-* returned by "row by row" reader with batch reader.
-*
-*/
-public class TestVectorizedORCReader {
-
-  private Configuration conf;
-  private FileSystem fs;
-  private Path testFilePath;
-
-  @Before
-  public void openFileSystem()  {
-    conf = new Configuration();
-    fs = FileSystem.getLocal(conf);
-    Path workDir = new Path(System.getProperty("test.tmp.dir",
-        "target" + File.separator + "test" + File.separator + "tmp"));
-    fs.setWorkingDirectory(workDir);
-    testFilePath = new Path("TestVectorizedORCReader.testDump.orc");
-    fs.delete(testFilePath, false);
-  }
-
-  @SuppressWarnings("unused")
-  static class MyRecord {
-    private final bool bo;
-    private final Byte by;
-    private final Integer i;
-    private final Long l;
-    private final Short s;
-    private final Double d;
-    private final String k;
-    private final Timestamp t;
-    private final Date dt;
-    private final HiveDecimal hd;
-
-    MyRecord(Boolean bo, Byte by, Integer i, Long l, Short s, Double d, String k,
-        Timestamp t, Date dt, HiveDecimal hd) {
-      this.bo = bo;
-      this.by = by;
-      this.i = i;
-      this.l = l;
-      this.s = s;
-      this.d = d;
-      this.k = k;
-      this.t = t;
-      this.dt = dt;
-      this.hd = hd;
-    }
-  }
-
-  [Fact]
-  public void createFile()  {
-    ObjectInspector inspector;
-    synchronized (TestVectorizedORCReader.class) {
-      inspector = ObjectInspectorFactory.getReflectionObjectInspector
-          (MyRecord.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
-    }
-
-    Writer writer = OrcFile.createWriter(fs, testFilePath, conf, inspector,
-        100000, CompressionKind.ZLIB, 10000, 10000);
-    Random r1 = new Random(1);
-    String[] words = new String[] {"It", "was", "the", "best", "of", "times,",
-        "it", "was", "the", "worst", "of", "times,", "it", "was", "the", "age",
-        "of", "wisdom,", "it", "was", "the", "age", "of", "foolishness,", "it",
-        "was", "the", "epoch", "of", "belief,", "it", "was", "the", "epoch",
-        "of", "incredulity,", "it", "was", "the", "season", "of", "Light,",
-        "it", "was", "the", "season", "of", "Darkness,", "it", "was", "the",
-        "spring", "of", "hope,", "it", "was", "the", "winter", "of", "despair,",
-        "we", "had", "everything", "before", "us,", "we", "had", "nothing",
-        "before", "us,", "we", "were", "all", "going", "direct", "to",
-        "Heaven,", "we", "were", "all", "going", "direct", "the", "other",
-        "way"};
-    String[] dates = new String[] {"1991-02-28", "1970-01-31", "1950-04-23"};
-    String[] decimalStrings = new String[] {"234.443", "10001000", "0.3333367", "67788798.0", "-234.443",
-        "-10001000", "-0.3333367", "-67788798.0", "0"};
-    for (int i = 0; i < 21000; ++i) {
-      if ((i % 7) != 0) {
-        writer.addRow(new MyRecord(((i % 3) == 0), (byte)(i % 5), i, (long) 200, (short) (300 + i), (double) (400 + i),
-            words[r1.nextInt(words.length)], new Timestamp(Calendar.getInstance().getTime().getTime()),
-            Date.valueOf(dates[i % 3]), HiveDecimal.create(decimalStrings[i % decimalStrings.length])));
-      } else {
-        writer.addRow(new MyRecord(null, null, i, (long) 200, null, null, null, null, null, null));
-      }
-    }
-    writer.close();
-    checkVectorizedReader();
-  }
-
-  private void checkVectorizedReader()  {
-
-    Reader vreader = OrcFile.createReader(testFilePath,
-        OrcFile.readerOptions(conf));
-    Reader reader = OrcFile.createReader(testFilePath,
-        OrcFile.readerOptions(conf));
-    RecordReaderImpl vrr = (RecordReaderImpl) vreader.rows();
-    RecordReaderImpl rr = (RecordReaderImpl) reader.rows();
-    VectorizedRowBatch batch = null;
-    OrcStruct row = null;
-
-    // Check Vectorized ORC reader against ORC row reader
-    while (vrr.hasNext()) {
-      batch = vrr.nextBatch(batch);
-      for (int i = 0; i < batch.size; i++) {
-        row = (OrcStruct) rr.next(row);
-        for (int j = 0; j < batch.cols.length; j++) {
-          Object a = (row.getFieldValue(j));
-          ColumnVector cv = batch.cols[j];
-          // if the value is repeating, use row 0
-          int rowId = cv.isRepeating ? 0 : i;
-
-          // make sure the null flag agrees
-          if (a == null) {
-            Assert.Assert.Equal(true, !cv.noNulls && cv.isNull[rowId]);
-          } else if (a instanceof BooleanWritable) {
-
-            // bool values are stores a 1's and 0's, so convert and compare
-            Long temp = (long) (((BooleanWritable) a).get() ? 1 : 0);
-            long b = ((LongColumnVector) cv).vector[rowId];
-            Assert.Assert.Equal(temp.toString(), Long.toString(b));
-          } else if (a instanceof TimestampWritable) {
-            // Timestamps are stored as long, so convert and compare
-            TimestampWritable t = ((TimestampWritable) a);
-            // Timestamp.getTime() is overriden and is 
-            // long time = super.getTime();
-            // return (time + (nanos / 1000000));
-            Long timeInNanoSec = (t.getTimestamp().getTime() * 1000000)
-                + (t.getTimestamp().getNanos() % 1000000);
-            long b = ((LongColumnVector) cv).vector[rowId];
-            Assert.Assert.Equal(timeInNanoSec.toString(), Long.toString(b));
-
-          } else if (a instanceof DateWritable) {
-            // Dates are stored as long, so convert and compare
-
-            DateWritable adt = (DateWritable) a;
-            long b = ((LongColumnVector) cv).vector[rowId];
-            Assert.Assert.Equal(adt.get().getTime(),
-                DateWritable.daysToMillis((int) b));
-
-          } else if (a instanceof HiveDecimalWritable) {
-            // Decimals are stored as BigInteger, so convert and compare
-            HiveDecimalWritable dec = (HiveDecimalWritable) a;
-            HiveDecimalWritable b = ((DecimalColumnVector) cv).vector[i];
-            Assert.Assert.Equal(dec, b);
-
-          } else if (a instanceof DoubleWritable) {
-
-            double b = ((DoubleColumnVector) cv).vector[rowId];
-            Assert.Equal(a.toString(), Double.toString(b));
-          } else if (a instanceof Text) {
-            BytesColumnVector bcv = (BytesColumnVector) cv;
-            Text b = new Text();
-            b.set(bcv.vector[rowId], bcv.start[rowId], bcv.length[rowId]);
-            Assert.Equal(a, b);
-          } else if (a instanceof IntWritable ||
-              a instanceof LongWritable ||
-              a instanceof ByteWritable ||
-              a instanceof ShortWritable) {
-            Assert.Equal(a.toString(),
-                Long.toString(((LongColumnVector) cv).vector[rowId]));
-          } else {
-            Assert.Equal("huh", a.getClass().getName());
-          }
+            public MyRecord(bool? bo, byte? by, int? i, long? l, short? s, double? d, string k,
+                Timestamp? t, Date? dt, HiveDecimal hd)
+            {
+                this.bo = bo;
+                this.by = by;
+                this.i = i;
+                this.l = l;
+                this.s = s;
+                this.d = d;
+                this.k = k;
+                this.t = t;
+                this.dt = dt;
+                this.hd = hd;
+            }
         }
-      }
 
-      // Check repeating
-      Assert.Assert.Equal(false, batch.cols[0].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[1].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[2].isRepeating);
-      Assert.Assert.Equal(true, batch.cols[3].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[4].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[5].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[6].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[7].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[8].isRepeating);
-      Assert.Assert.Equal(false, batch.cols[9].isRepeating);
+        const string testFileName = "TestVectorizedORCReader.orc";
 
-      // Check non null
-      Assert.Assert.Equal(false, batch.cols[0].noNulls);
-      Assert.Assert.Equal(false, batch.cols[1].noNulls);
-      Assert.Assert.Equal(true, batch.cols[2].noNulls);
-      Assert.Assert.Equal(true, batch.cols[3].noNulls);
-      Assert.Assert.Equal(false, batch.cols[4].noNulls);
-      Assert.Assert.Equal(false, batch.cols[5].noNulls);
-      Assert.Assert.Equal(false, batch.cols[6].noNulls);
-      Assert.Assert.Equal(false, batch.cols[7].noNulls);
-      Assert.Assert.Equal(false, batch.cols[8].noNulls);
-      Assert.Assert.Equal(false, batch.cols[9].noNulls);
+        public TestVectorizedORCReader()
+            : base(testFileName)
+        {
+        }
+
+        [Fact]
+        public void createFile()
+        {
+            ObjectInspector inspector = ObjectInspectorFactory.getReflectionObjectInspector(typeof(MyRecord));
+
+            using (Stream file = File.OpenWrite(testFilePath))
+            using (Writer writer = OrcFile.createWriter(testFilePath, file, OrcFile.writerOptions(conf)
+                .stripeSize(100000)
+                .compress(CompressionKind.ZLIB)
+                .inspector(inspector)
+                .bufferSize(10000)
+                .rowIndexStride(10000)))
+            {
+                Random r1 = new Random(1);
+                string[] words = TestHelpers.words;
+                string[] dates = new string[] { "1991-02-28", "1970-01-31", "1950-04-23" };
+                string[] decimalStrings = new string[]
+            {
+                "234.443", "10001000", "0.3333367", "67788798.0", "-234.443",
+                "-10001000", "-0.3333367", "-67788798.0", "0"
+            };
+                for (int i = 0; i < 21000; ++i)
+                {
+                    if ((i % 7) != 0)
+                    {
+                        writer.addRow(new MyRecord(((i % 3) == 0), (byte)(i % 5), i, (long)200, (short)(300 + i), (double)(400 + i),
+                            words[r1.Next(words.Length)], new Timestamp(DateTime.Now),
+                            Date.Parse(dates[i % 3]), HiveDecimal.Parse(decimalStrings[i % decimalStrings.Length])));
+                    }
+                    else
+                    {
+                        writer.addRow(new MyRecord(null, null, i, (long)200, null, null, null, null, null, null));
+                    }
+                }
+                writer.close();
+            }
+            checkVectorizedReader();
+        }
+
+        private void checkVectorizedReader()
+        {
+            Reader vreader = OrcFile.createReader(testFilePath,
+                OrcFile.readerOptions(conf));
+            Reader reader = OrcFile.createReader(testFilePath,
+                OrcFile.readerOptions(conf));
+            RecordReaderImpl vrr = (RecordReaderImpl)vreader.rows();
+            RecordReaderImpl rr = (RecordReaderImpl)reader.rows();
+            VectorizedRowBatch batch = null;
+
+            // Check Vectorized ORC reader against ORC row reader
+            while (vrr.hasNext())
+            {
+                batch = vrr.nextBatch(batch);
+                for (int i = 0; i < batch.size; i++)
+                {
+                    OrcStruct row = (OrcStruct)rr.next();
+                    for (int j = 0; j < batch.cols.Length; j++)
+                    {
+                        object a = (row.getFieldValue(j));
+                        ColumnVector cv = batch.cols[j];
+                        // if the value is repeating, use row 0
+                        int rowId = cv.isRepeating ? 0 : i;
+
+                        // make sure the null flag agrees
+                        if (a == null)
+                        {
+                            Assert.True(!cv.noNulls && cv.isNull[rowId]);
+                        }
+                        else if (a is bool)
+                        {
+
+                            // bool values are stores a 1's and 0's, so convert and compare
+                            long temp = (bool)a ? 1 : 0;
+                            long b = ((LongColumnVector)cv).vector[rowId];
+                            Assert.Equal(temp.ToString(), b.ToString());
+                        }
+                        else if (a is Timestamp)
+                        {
+                            // Timestamps are stored as long, so convert and compare
+                            Timestamp t = (Timestamp)a;
+                            // Timestamp.getTime() is overriden and is 
+                            // long time = super.getTime();
+                            // return (time + (nanos / 1000000));
+                            long timeInNanoSec = (t.Milliseconds * 1000000)
+                                + (t.getNanos() % 1000000);
+                            long b = ((LongColumnVector)cv).vector[rowId];
+                            Assert.Equal(timeInNanoSec.ToString(), b.ToString());
+
+                        }
+                        else if (a is Date)
+                        {
+                            // Dates are stored as long, so convert and compare
+
+                            Date adt = (Date)a;
+                            long b = ((LongColumnVector)cv).vector[rowId];
+                            // Assert.Equal(adt, Date.daysToMillis((int)b));
+                            Assert.Equal(adt.Days, (int)b);
+                        }
+                        else if (a is HiveDecimal)
+                        {
+                            // Decimals are stored as BigInteger, so convert and compare
+                            HiveDecimal dec = (HiveDecimal)a;
+                            HiveDecimal b = ((DecimalColumnVector)cv).vector[i];
+                            Assert.Equal(dec, b);
+                        }
+                        else if (a is double)
+                        {
+                            double b = ((DoubleColumnVector)cv).vector[rowId];
+                            Assert.Equal(a.ToString(), b.ToString());
+                        }
+                        else if (a is string)
+                        {
+                            BytesColumnVector bcv = (BytesColumnVector)cv;
+                            string b = Encoding.UTF8.GetString(bcv.vector[rowId], bcv.start[rowId], bcv.length[rowId]);
+                            Assert.Equal((string)a, b);
+                        }
+                        else if (a is int || a is long || a is byte || a is short)
+                        {
+                            Assert.Equal(a.ToString(),
+                                ((LongColumnVector)cv).vector[rowId].ToString());
+                        }
+                        else
+                        {
+                            Assert.True(false);
+                        }
+                    }
+                }
+
+                // Check repeating
+                Assert.Equal(false, batch.cols[0].isRepeating);
+                Assert.Equal(false, batch.cols[1].isRepeating);
+                Assert.Equal(false, batch.cols[2].isRepeating);
+                Assert.Equal(true, batch.cols[3].isRepeating);
+                Assert.Equal(false, batch.cols[4].isRepeating);
+                Assert.Equal(false, batch.cols[5].isRepeating);
+                Assert.Equal(false, batch.cols[6].isRepeating);
+                Assert.Equal(false, batch.cols[7].isRepeating);
+                Assert.Equal(false, batch.cols[8].isRepeating);
+                Assert.Equal(false, batch.cols[9].isRepeating);
+
+                // Check non null
+                Assert.Equal(false, batch.cols[0].noNulls);
+                Assert.Equal(false, batch.cols[1].noNulls);
+                Assert.Equal(true, batch.cols[2].noNulls);
+                Assert.Equal(true, batch.cols[3].noNulls);
+                Assert.Equal(false, batch.cols[4].noNulls);
+                Assert.Equal(false, batch.cols[5].noNulls);
+                Assert.Equal(false, batch.cols[6].noNulls);
+                Assert.Equal(false, batch.cols[7].noNulls);
+                Assert.Equal(false, batch.cols[8].noNulls);
+                Assert.Equal(false, batch.cols[9].noNulls);
+            }
+            Assert.Equal(false, rr.hasNext());
+        }
     }
-    Assert.Assert.Equal(false, rr.hasNext());
-  }
 }
